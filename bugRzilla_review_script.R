888e0a08b9de5a52e0d2e5e87b5cdd05106bc565
kpiyush04 <04kpiyush@gmail.com>
2021-07-22 07:28:56 +0530

    Visualization on Bugs table

--- bugRzilla_review_script.R
+++ bugRzilla_review_script.R
@@ -3,13 +3,16 @@ library(dplyr)
 library(dbplyr)
 library(RMySQL)
 library(DBI)
-library(tibble)
 library(DT)
 library(tidyverse)
-library(lubridate)
+library(ggplot2)
+library(plotly)
 
+#######################################
 # Connect bugRzilla SQL Database with R
-# connecting MySQL with R
+#######################################
+
+# Connecting R with MySQL
 con <- dbConnect(
     MySQL(),
     dbname='bugRzilla', # change the database name to your database name
@@ -17,90 +20,60 @@ con <- dbConnect(
     password='1204', # update your password
     host='localhost',
     port=3306)
-#  Accessing Tables from the Database
-src_dbi(con)
 
+#  Accessing Tables names from the Database
+DBI::dbListTables(con)
+
+##################################################
+# Data Exploartion of Bugs Table from the Database
+##################################################
+bugs_df <- tbl(con, "bugs")
 
 # View the "bugs" tables from the database
 bugs_db <- tbl(con, "bugs")
 bugs_data <- bugs_db %>%
     select(everything())
 
-datatable(as.data.frame(bugs_data), options = list(scrollY = "645px"),
-          class = "display", fillContainer = T, width = NULL,
-          height = NULL, style = "default", editable = FALSE)
+# Converting `bugs_df` to `dataframe` and showing the `datatable`
+bugs_df <- as.data.frame(bugs_df)
+datatable(bugs_df, options = list(scrollX = TRUE,
+                                  pageLength = 5, lengthMenu = c(5, 10, 50, 100)))
 
-# Data Wrangling
-bugs <- bugs_db %>%
-    mutate_all(as.character) %>%  # converting all columns data types to "char"
-    as_tibble %>% # must be a data frame for na_if to work
-    na_if("") %>% #replace empty strings with NA
-    # converting all "char" data types to int leaving the "char" datatype
-    mutate_at(vars(-one_of(c("bug_severity", "bug_status", "short_desc",
-                             "priority", "rep_platform", "bug_file_loc",
-                             "lastdiffed", "op_sys", "version",
-                             "resolution", "target_milestone", "creation_ts",
-                             "delta_ts", "status_whiteboard", "deadline"))), as.integer) %>%
-    # converting all columns data types to "Date" leaving the "char" and "int" datatype
-    mutate_at(vars(-one_of(c("bug_severity", "bug_status", "short_desc",
-                             "bug_file_loc", "priority", "rep_platform",
-                             "version", "resolution", "target_milestone",
-                             "bug_id", "assigned_to", "op_sys",
-                             "status_whiteboard", "product_id", "reporter",
-                             "component_id", "qa_contact", "votes",
-                             "everconfirmed", "reporter_accessible",
-                             "cclist_accessible", "estimated_time",
-                             "remaining_time"))), as.Date)
+#for quick view of the datatypes and the structure of data
+glimpse(bugs_df)
 
-# removing null or empty columns
-bugs <- bugs %>%
-    select(-c("target_milestone", "qa_contact", "status_whiteboard",
-              "remaining_time", "remaining_time",  "estimated_time"))
+#converting the required fields in the correct datatype format
+bugs_df <- bugs_df %>%
+    mutate_at(vars("creation_ts", "delta_ts", "lastdiffed", "deadline"), as.Date)
 
-bugs <- bugs %>%
-    select(everything())
-datatable(as.data.frame(bugs), options = list(scrollY = "645px"),
-          class = "display", fillContainer = T, width = NULL,
-          height = NULL, style = "default", editable = FALSE)
+glimpse(bugs_df)
+
+################
+# Visualizations
+################
+
+# Line plot for creation_ts, delta_ts, deadline
+creation <- bugs_df$creation_ts
+delta <- bugs_df$delta_ts
+deadline <- bugs_df$deadline
+last_diffed <- bugs_df$lastdiffed
+bug_id <- bugs_df$bug_id
+component_id <- bugs_df$component_id
+resolution <- bugs_df$resolution
 
 
-# View the "attachments" Table from the database
-attach_df <- attachments_db %>%
-    as_tibble %>% # must be a data frame for na_if to work
-    na_if("") %>% #replace empty strings with NA
-    # converting all columns data types to "Date" leaving the "char" and "int" datatype
-    mutate_at(vars(-one_of(c("attach_id", "bug_id", "description", "mimetype",
-                             "ispatch", "filename", "submitter_id",
-                             "isobsolete", "isprivate"))), as.Date)
+data <- data.frame(bug_id, creation)
+fig <- plot_ly(data, y = ~bug_id, x = ~creation, type = 'scatter', mode = 'markers')
+fig
 
-attach_df <- attach_df %>%
-    select(-c("isprivate"))
-attach_df <- attach_df %>%
-    select(everything())
+data <- data.frame(bug_id, delta)
+fig <- plot_ly(data, y = ~bug_id, x = ~delta, type = 'scatter', mode = 'markers')
+fig
 
-datatable(as.data.frame(attach_df), options = list(scrollY = "565px"),
-          class = "display", fillContainer = T, width = NULL,
-          height = NULL, style = "default", editable = FALSE)
+data <- data.frame(bug_id, deadline)
+fig <- plot_ly(data, y = ~bug_id, x = ~deadline, type = 'scatter', mode = 'markers')
+fig
 
-
-# View the "bugs_activity" Table from the database
-bugs_activity_db <- tbl(con, "bugs_activity")
-bugs_activity_data <- bugs_activity_db %>%
-    select(everything())
-
-datatable(as.data.frame(bugs_activity_data), options = list(scrollY = "543px"),
-          class = "display", fillContainer = T, width = NULL,
-          height = NULL, style = "default", editable = FALSE)
-
-# Data Wrangling
-bugs_act <- bugs_activity_db %>%
-    as_tibble %>% # must be a data frame for na_if to work
-    na_if("") %>% #replace empty strings with NA
-    mutate_at(vars(-one_of(c("attach_id", "bug_id", "who", "fieldid", "added",
-                             "comment_id", "removed", "id"))), as.Date)
-
-bugs_act <- bugs_act %>%
-    select(everything())
-datatable(as.data.frame(bugs_act), options = list(scrollY = "543px"),
-          class = "display", fillContainer = T, width = NULL,
-          height = NULL, style = "default", editable = FALSE)
+data <- data.frame(bug_id, resolution)
+fig <- plot_ly(data, x = ~resolution, y = ~bug_id, name = 'creation', type = 'bar')
+fig
