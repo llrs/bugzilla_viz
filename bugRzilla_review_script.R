85fd96eb6c7f99c7fce90182be6bb24aefb7235e
kpiyush04 <04kpiyush@gmail.com>
2021-07-27 22:38:22 +0530

    Explored the bugs data_table and updatd the visualization and description

--- bugRzilla_review_script.R
+++ bugRzilla_review_script.R
@@ -25,55 +25,103 @@ con <- dbConnect(
 DBI::dbListTables(con)
 
 ##################################################
-# Data Exploartion of Bugs Table from the Database
+# Data Exploration of Bugs Table from the Database
 ##################################################
 bugs_df <- tbl(con, "bugs")
 
-# View the "bugs" tables from the database
-bugs_db <- tbl(con, "bugs")
-bugs_data <- bugs_db %>%
-    select(everything())
-
-# Converting `bugs_df` to `dataframe` and showing the `datatable`
-bugs_df <- as.data.frame(bugs_df)
-datatable(bugs_df, options = list(scrollX = TRUE,
-                                  pageLength = 5, lengthMenu = c(5, 10, 50, 100)))
-
 #for quick view of the datatypes and the structure of data
 glimpse(bugs_df)
+
+# Converting `bugs_df` to `dataframe`
+bugs_df <- as.data.frame(bugs_df)
 
 #converting the required fields in the correct datatype format
 bugs_df <- bugs_df %>%
     mutate_at(vars("creation_ts", "delta_ts", "lastdiffed", "deadline"), as.Date)
 
+# Taking the columns which are useful
+bugs_df <- bugs_df %>%
+    select("bug_id", "bug_severity", "bug_status", "creation_ts", "delta_ts",
+           "op_sys", "priority", "resolution", "component_id",
+           "version", "lastdiffed", "deadline")
+
+#for quick view of the datatypes and the structure of data
 glimpse(bugs_df)
+
+#showing the `datatable`
+datatable(head(bugs_df, 10), options = list(scrollX = TRUE))
 
 ################
 # Visualizations
 ################
 
-# Line plot for creation_ts, delta_ts, deadline
-creation <- bugs_df$creation_ts
-delta <- bugs_df$delta_ts
-deadline <- bugs_df$deadline
-last_diffed <- bugs_df$lastdiffed
+# Plotting the Time Series graph with the bug_id and creation_ts
 bug_id <- bugs_df$bug_id
-component_id <- bugs_df$component_id
-resolution <- bugs_df$resolution
-
-
+creation <- bugs_df$creation_ts
 data <- data.frame(bug_id, creation)
-fig <- plot_ly(data, y = ~bug_id, x = ~creation, type = 'scatter', mode = 'markers')
-fig
+fig1 <- plot_ly(data,
+                x = ~creation,
+                y = ~bug_id,
+                type = 'scatter',
+                mode = 'markers')
+fig1
 
+# Plotting the Bar graph and adding Trace of Time-Series graph with bug_id and creation_ts to see the spread
+fig1 <- plot_ly(data,
+                x = ~creation,
+                y = ~bug_id,
+                type = 'bar',
+                name = "bug_creation bar")
+fig1 <- fig1 %>%
+    add_trace(fig1,
+              type = 'scatter',
+              mode='lines+markers',
+              name = "bug_creation Time_series")
+fig1
+
+# Plotting the Time Series graph with the bug_id and delta_ts
+delta <- bugs_df$delta_ts
 data <- data.frame(bug_id, delta)
-fig <- plot_ly(data, y = ~bug_id, x = ~delta, type = 'scatter', mode = 'markers')
-fig
+fig2 <- plot_ly(data,
+                x = ~delta,
+                y = ~bug_id,
+                type = 'scatter',
+                mode = 'markers')
+fig2
 
+# Plotting the Time Series graph with the bug_id and deadline
+deadline <- bugs_df$deadline
 data <- data.frame(bug_id, deadline)
-fig <- plot_ly(data, y = ~bug_id, x = ~deadline, type = 'scatter', mode = 'markers')
-fig
+fig3 <- plot_ly(data,
+                y = ~bug_id,
+                x = ~deadline,
+                type = 'scatter',
+                mode = 'markers')
+fig3
 
+# Plotting bar graph with bug_id and resolution
+resolution <- bugs_df$resolution
 data <- data.frame(bug_id, resolution)
-fig <- plot_ly(data, x = ~resolution, y = ~bug_id, name = 'creation', type = 'bar')
-fig
+fig4 <- plot_ly(data,
+                x = ~resolution,
+                y = ~bug_id,
+                type = 'bar')
+fig4
+
+# Plotting bar graph with bug_id and bug_status
+bug_status <- bugs_df$bug_status
+data <- data.frame(bug_id, bug_status)
+fig5 <- plot_ly(data,
+                x = ~bug_status,
+                y = ~bug_id,
+                type = 'bar')
+fig5
+
+# Plotting bar graph with bug_id and bug_severity
+bug_severity <- bugs_df$bug_severity
+data <- data.frame(bug_id, bug_severity)
+fig6 <- plot_ly(data,
+                x = ~bug_severity,
+                y = ~bug_id,
+                type = 'bar')
+fig6
